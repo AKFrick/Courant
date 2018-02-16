5953fcc63357e0b79e26d748aa98f4975beb1ab2
Alex Frick <ak.frick@outlook.com>
2018-02-16 09:53:48 +0500

    project has been created

---
+++ app/src/main/java/com/akf/courant/MainActivity.java
@@ -0,0 +1,27 @@
+package com.akf.courant;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button Start;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Start = findViewById(R.id.Start);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
