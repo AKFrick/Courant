f891edae666b277af4fa94c75a55a28218e9346b
Alex Frick <ak.frick@outlook.com>
2018-02-16 10:38:33 +0500

    created new launcher icon

--- app/src/main/java/com/akf/courant/MainActivity.java
+++ app/src/main/java/com/akf/courant/MainActivity.java
@@ -1,6 +1,9 @@
 package com.akf.courant;
 
+import android.app.AlarmManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    AlarmManager alarmManager;
 
     Button Start;
     @Override
@@ -21,7 +26,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
     public void onClick(View view) {
+        MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.svist);
+        mp.start();
 
     }
 }
